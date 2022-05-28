1e9b04edfb82579492dc50dd33cc33833f7d89c8
eternalfever <anastasia.kuzminaaa@gmail.com>
2022-05-28 12:18:11 +0300

    Рендер JSX

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,25 @@
 import { subscribe, unsubscribe } from './resources/API';
+import { useEffect, useState } from 'react';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [message, setMessage] = useState(-1);
+
+    useEffect(() => {
+        const cb = (message: number) => {
+            setMessage(message);
+        };
+        subscribe(props.sourceId, cb);
+
+        const result = () => {
+            unsubscribe(props.sourceId, cb);
+            setMessage(-1);
+        };
+        return result;
+    }, [props.sourceId]);
+
+    return (
+        <div>
+            {props.sourceId}: {message}
+        </div>
+    );
 }
